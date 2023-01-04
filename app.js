9663f9079467f7f7957e2fe02090dc37e82be596
Rajesh Bagguva <rajeshbagguvaiiitn@gmail.com>
2023-01-04 08:51:30 +0530

    Class_02

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const header1 = React.createElement(
   "h1",
   { className: "60px" },
@@ -5,9 +8,12 @@ const header1 = React.createElement(
 );
 const header2 = React.createElement("h2", {}, "This is h2 in REACT!!!");
 const header3 = React.createElement("h3", {}, "This is h3 in REACT!!!");
-console.log(header1);
+const container = React.createElement("div", { id: "container" }, [
+  header1,
+  header2,
+  header3,
+]);
+// console.log(header1);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(header3);
-root.render(header2);
-root.render(header1);
+// console.log(root);
+root.render(container);
